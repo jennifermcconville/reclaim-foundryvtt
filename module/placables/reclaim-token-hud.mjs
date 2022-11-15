9a2eb1bd25574965142353966c44b60d72e5bf74
Adam Politanski @ HiQ <adam.politanski@hiq.se>
2022-11-15 16:14:49 +0100

    Added a delete token button that calls an empty function

--- module/placables/reclaim-token-hud.mjs
+++ module/placables/reclaim-token-hud.mjs
@@ -4,6 +4,14 @@
 
 export class ReclaimTokenHUD extends TokenHUD {
 
+   /** @override
+  static get defaultOptions() {
+     return foundry.utils.mergeObject(super.defaultOptions, {
+        id: "token-hud",
+        template: "systems/reclaim/templates/reclaim-token-hud.html"
+     });
+  } */
+
    /** @override */
    getData(options) {
       let data = super.getData(options);
@@ -30,12 +38,57 @@ export class ReclaimTokenHUD extends TokenHUD {
 
    /** @override */
    async _render(...args) {
+      var effectsButton = this.element.find('div.col.right').find('div.control-icon[data-action="effects"]');
+
+
       await super._render(...args);
 
       // hiding elements via jQuerry since they are not made conditional in the template 
       this.element.find('div.col.left').find('div.control-icon[data-action="target"]').hide()
       this.element.find('div.col.left').find('div.attribute.elevation').hide();
 
-      this.element.find('div.col.right').find('div.control-icon[data-action="effects"]').hide(); 
+      var effectsButton = this.element.find('div.col.right').find('div.control-icon[data-action="effects"]');
+      effectsButton.hide();
+
+      var deleteButton = $(`
+                           <div class="control-icon" data-action="delete">
+                              <img src="icons/svg/cancel.svg" width="36" height="36" title="Delete token"> 
+                           </div>
+                        `);
+
+      deleteButton.insertBefore(effectsButton);
+      deleteButton.click(this._onClickDelete.bind(this));
+
+
+
+
+      //.click(this._onClickControl.bind(this));
+
+      //adds the Delete button for all users
+
+      /** icon - cancel? style: fill red 
+      *  <div class="control-icon" data-action="delete">
+      *      <img src="icons/svg/cancel.svg" width="36" height="36" title="Delete token">
+      *  </div>   
+      */
+   }
+
+   // super._onClickControl(event);
+   // if ( event.defaultPrevented ) return;
+   // const button = event.currentTarget;
+   // switch ( button.dataset.action ) {
+   //   case "config":
+   //     return this._onTokenConfig(event);
+
+   _onClickDelete(event) {
+      if (event.defaultPrevented) return;
+      const button = event.currentTarget;
+      if(button.dataset.action == "delete") 
+         return this._onDeleteToken(event);
+   }
+
+   _onDeleteToken(event) {
+      console.log("Delete token now");
+
    }
 }
