c9632fd570963b7859acad6f0e97d19fab32ea5b
Adam Politanski @ HiQ <adam.politanski@hiq.se>
2022-12-06 15:17:27 +0100

    Added a Change Token button to Token HUD

--- module/documents/token.mjs
+++ module/documents/token.mjs
@@ -4,26 +4,26 @@
  */
 
 export class ReclaimToken extends Token {
-  //** @override */
+  //* @override */
   _onCreate(options, userId) {
-    // function body copied from token.js row 1523 just to change camera panning
-    
+    // Function body copied from token.js row 1523 just to change camera panning
+
     // Start by drawing the newly created token
     this.draw().then(() => {
 
       // Draw vision for the new token
       const refreshVision = this.document.sight.enabled && this.observer;
       const refreshLighting = this.emitsLight;
-      if ( refreshVision || refreshLighting ) {
-        this.updateSource({defer: true});
-        canvas.perception.update({refreshVision, refreshLighting}, true);
+      if (refreshVision || refreshLighting) {
+        this.updateSource({ defer: true });
+        canvas.perception.update({ refreshVision, refreshLighting }, true);
       }
 
       // Assume token control
-      if ( !game.user.isGM && this.isOwner && !this.document.hidden ) this.control({pan: false});
+      if (!game.user.isGM && this.isOwner && !this.document.hidden) this.control({ pan: false });
 
       // Update visibility of objects in the Scene
-      if ( !refreshVision ) canvas.effects.visibility.restrictVisibility();
+      if (!refreshVision) canvas.effects.visibility.restrictVisibility();
     });
   }
 }
--- module/placables/reclaim-token-hud.mjs
+++ module/placables/reclaim-token-hud.mjs
@@ -48,20 +48,34 @@ export class ReclaimTokenHUD extends TokenHUD {
                         `);
 
     let changeButton = $(`
-                          <div class="control-icon" data-action="delete">
-                        <img src="icons/svg/cancel.svg" width="36" height="36" title="Delete token"> 
-    </div>
- `);
+                          <div class="control-icon" data-action="change">
+                            <img src="icons/svg/direction.svg" width="36" height="36" title="Change resource"> 
+                          </div>
+                        `);
 
 
     deleteButton.insertBefore(effectsButton);
+
+
     // Connecting click listener manually as other buttons get connected in super._render call
     // which re-creates the html, so our button needs to be added afterwards
     deleteButton.click(this._onClickDelete.bind(this));
+
+    if (this?.object?.actor?.type === `resource`) {
+      changeButton.insertAfter(deleteButton);
+      changeButton.click(this._onClickChange.bind(this));
+    }
   }
 
+  /**
+   * Click handler for Delete button, removes current token from scene.
+   *
+   * @param {MouseEvent} event
+   * @private
+   */
   _onClickDelete(event) {
     // Following the pattern of other on click handles in super class
+    super._onClickControl(event);
     if (event.defaultPrevented) return;
     const button = event.currentTarget;
     if (button.dataset.action === `delete`) return this._onDeleteToken(event);
@@ -69,5 +83,55 @@ export class ReclaimTokenHUD extends TokenHUD {
 
   _onDeleteToken(event) {
     canvas.scene.deleteEmbeddedDocuments(`Token`, [this.object.id]);
+  }
+
+  /**
+   * Click handler for Change button, associates the token to the next Resource Actor in the folder.
+   *
+   * @param {MouseEvent} event
+   * @private
+   */
+  _onClickChange(event) {
+    super._onClickControl(event);
+    if (event.defaultPrevented) return;
+    const button = event.currentTarget;
+    if (button.dataset.action === `change`) return this._onChangeToken(event);
+  }
+
+  async _onChangeToken(event) {
+    if (this.object.actor.type !== `resource`) return;
+
+    console.debug(`Changing token...`);
+    const token = this.object;
+
+    // Get a list of all resource actors
+    // sort ?
+    let sortedResourceActors = game.actors.filter(actor => actor.type === `resource`).sort(a => a.name);
+
+    // Find next actor
+    let index = sortedResourceActors.findIndex(function (actor) {
+      if (actor.id === this) {
+        return true;
+      } else {
+        return false;
+      }
+    }, token.actor.id);
+
+    if (index < 0) {
+      throw Error(`Could not find tokens actor in actors list`);
+    }
+    index++;
+    if (index === sortedResourceActors) index = 0;
+    let nextActor = sortedResourceActors[index];
+
+    // Change this token association to the next actor in list
+    await token.document.update(
+      {
+        actorId: nextActor.id,
+        actor: nextActor,
+        texture: { src: nextActor.prototypeToken.texture.src },
+        id: nextActor.prototypeToken.id
+      },
+      { parent: token.parent.parent });
   }
 }
