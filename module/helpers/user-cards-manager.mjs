f744bb1b98d787a214025aff34b12eefc16d1ad8
Adam Politanski @ HiQ <adam.politanski@hiq.se>
2023-01-04 10:12:03 +0100

    Create card hand and assign to bottom mini-bar slot if missing

---
+++ module/helpers/user-cards-manager.mjs
@@ -0,0 +1,109 @@
+/* eslint-disable no-undef */
+
+import { RECLAIM } from "./config.mjs";
+import { ReclaimConnectedCards } from "../documents/connected-cards.mjs";
+
+export class UserCardsManager {
+
+  static async onReady() {
+    const cardFolders = await UserCardsManager.ensureCardFolders(
+      RECLAIM.FoundryFolderNames.CardHands,
+      RECLAIM.FoundryFolderNames.PlayerCardHands
+    );
+
+    await game.users.forEach( user => {
+      console.debug( `Processing user ${user.id}` );
+      let newHand = false;
+      let flag = user.getFlag( game.system.id, RECLAIM.Flags.UserCardHandId );
+      if ( !flag ) {
+        UserCardsManager.createUserCardHand( user, cardFolders );
+        newHand = true;
+      }
+
+      let userHandPromise = game.cards.find( cardHand => cardHand.id === flag );
+      if ( !userHandPromise ) {
+        userHandPromise = UserCardsManager.createUserCardHand( user, cardFolders );
+        newHand = true;
+      }
+
+      if ( userHandPromise.then ) {
+        userHandPromise.then( value => {
+          UserCardsManager.assingToMiniCardHand( user, value, newHand );
+        } );
+      } else {
+        UserCardsManager.assingToMiniCardHand( user, userHandPromise, newHand );
+      }
+    } );
+  }
+
+
+  /**
+   * Make sure that the hand is displayed as the bottom card collection of hand-mini-bar for this user.
+   * @param {User}user
+   * @param {Cards}hand
+   * @param {boolean}newlyCreated
+   */
+  static async assingToMiniCardHand( user, hand, newlyCreated ) {
+
+    // If we created a new card hand, assign to hand-mini-bar slot 0
+    if ( newlyCreated ) {
+      user.setFlag( UserCardsManager.HandMiniBarModule.moduleName, `CardsID-0`, hand.id );
+    }
+  }
+
+  /**
+   * Creates a new Hand and assigns this user it's Id via flag. Also makes sure mini-card-hand displays it as the bottom
+   *  card collection
+   *
+   * @param {User} user
+   * @param {CardFolders} cardFolders
+   * @returns {Cards}
+   */
+  static async createUserCardHand( user, cardFolders ) {
+    if ( !user || !cardFolders || !cardFolders.playerHands ) {
+      return;
+    }
+
+    const newHand = await ReclaimConnectedCards.create( {
+      name: `${user.name} Hand`,
+      folder: cardFolders.playerHands
+    } );
+
+    user.setFlag( game.system.id, RECLAIM.Flags.UserCardHandId, newHand.id );
+
+    return newHand;
+  }
+
+  /**
+   * Ensures that the Cards tab in Foundry has a folder to contain card hands
+   * with a subfolder from player card hands.
+   * @param {string} cardHandFolderName
+   * @param {string} playerHandSubfolderName
+   *
+   * @returns {CardFolders}
+   */
+  static async ensureCardFolders( cardHandFolderName, playerHandSubfolderName ) {
+    let cardHandsFolder = game.folders.find( folder => folder.name === cardHandFolderName );
+    let playerHandsFolder = game.folders.find( folder => folder.name === playerHandSubfolderName );
+
+    if ( !cardHandsFolder ) {
+      cardHandsFolder = Folder.create( {
+        name: cardHandFolderName,
+        type: `Cards` }
+      );
+    }
+
+    if ( !playerHandsFolder ) {
+      playerHandsFolder = await Folder.create( {
+        name: playerHandSubfolderName,
+        type: `Cards`,
+        parent: cardHandsFolder
+      } );
+    }
+
+    return {
+      cardHands: cardHandsFolder,
+      playerHands: playerHandsFolder
+    };
+  }
+}
--- module/reclaim.mjs
+++ module/reclaim.mjs
@@ -15,6 +15,7 @@ import { ReclaimTokenHUD } from "./placables/reclaim-token-hud.mjs";
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
 import { RECLAIM } from "./helpers/config.mjs";
+import { UserCardsManager } from "./helpers/user-cards-manager.mjs";
 
 /**
  * @typedef {Object} CardFolders
@@ -74,107 +75,7 @@ Hooks.once( `init`, async function() {
 
 Hooks.once( `ready`, async function() {
   game.canvas.hud.token = new ReclaimTokenHUD();
+  UserCardsManager.onReady();
+} ); // End hook ready
 
-  const cardFolders = await ensureCardFolders(
-    RECLAIM.FoundryFolderNames.CardHands,
-    RECLAIM.FoundryFolderNames.PlayerCardHands
-  );
-
-  await game.users.forEach( user => {
-    console.debug( `Processing user ${user.id}` );
-    let flag = user.getFlag( game.system.id, RECLAIM.Flags.UserCardHandId );
-    if ( !flag ) {
-      createUserCardHand( user, cardFolders );
-    }
-
-    let userHand = game.cards.find( cardHand => cardHand.id === flag );
-    if ( !userHand ) {
-      userHand = createUserCardHand( user, cardFolders );
-    }
-
-    assingToMiniCardHand( user, userHand );
-  } );
-
-  Hooks;
-} );
-
-Hooks.on( `renderApplication`, async function( ...args ) {
-  console.debug( `Heard a renderApplication hook!` );
-} );
-
-Hooks.on( `renderHandMiniBarWindow`, async function( ...args ) {
-  console.debug( `Heard a renderHandMiniBarWindow hook!` );
-} );
-
-
-/**
- * Make sure that the hand is displayed as the bottom card collection of hand-mini-bar for this user.
- * @param {User}user
- * @param {Cards}hand
- */
-async function assingToMiniCardHand( user, hand ) {
-  console.debug( `Assigning card hand ${hand.id} to user's ${user.id} mini bar...` );
-  HandMiniBarModule.handMiniBarList[ 0 ];
-}
-
-/**
- * Creates a new Hand and assigns this user it's Id via flag. Also makes sure mini-card-hand displays it as the bottom
- *  card collection
- *
- * @param {User} user
- * @param {CardFolders} cardFolders
- * @returns {Cards}
- */
-async function createUserCardHand( user, cardFolders ) {
-  console.debug( `Creating new card hand for user ${user.id}.` );
-  if ( !user || !cardFolders || !cardFolders.playerHands ) {
-    return;
-  }
-
-  const newHand = await ReclaimConnectedCards.create( {
-    name: `${user.name} Hand`,
-    folder: cardFolders.playerHands
-  } );
-
-  user.setFlag( game.system.id, RECLAIM.Flags.UserCardHandId, newHand.id );
-
-  return newHand;
-}
-
-/**
- * Ensures that the Cards tab in Foundry has a folder to contain card hands
- * with a subfolder from player card hands.
- * @param {string} cardHandFolderName
- * @param {string} playerHandSubfolderName
- *
- * @returns {CardFolders}
- */
-async function ensureCardFolders( cardHandFolderName, playerHandSubfolderName ) {
-  let cardHandsFolder = game.folders.find( folder => folder.name === cardHandFolderName );
-  let playerHandsFolder = game.folders.find( folder => folder.name === playerHandSubfolderName );
-
-  if ( !cardHandsFolder ) {
-    cardHandsFolder = Folder.create( {
-      name: cardHandFolderName,
-      type: `Cards` }
-    );
-  }
-
-  if ( !playerHandsFolder ) {
-    playerHandsFolder = await Folder.create( {
-      name: playerHandSubfolderName,
-      type: `Cards`,
-      parent: cardHandsFolder
-    } );
-  }
-
-  return {
-    cardHands: cardHandsFolder,
-    playerHands: playerHandsFolder
-  };
-
-
-  // TODO give / create a card hand to the user
-  // make sure his mini-card-hand displays it
-}
 
